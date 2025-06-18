9f6367e2c0a2d6526198b2de006b99f3528654db
Huy Quang <haduyenquanghuy@gmail.com>
2025-06-18 15:51:34 +0700

    add firebase

--- BetterMeGoalApp/BetterMeGoalAppApp.swift
+++ BetterMeGoalApp/BetterMeGoalAppApp.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct BetterMeGoalAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
@@ -18,3 +21,12 @@ struct BetterMeGoalAppApp: App {
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
